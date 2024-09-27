5061456b9ede4b8e83f3720bd6e06d1b182698f6
Matias-Satina <109638715+dbetania2@users.noreply.github.com>
2024-09-27 06:42:28 -0300

    Intento de eliminar  la doble carpeta src en topic2

--- ShopiShopping/src/main/java/com/shopi/shopping/factories/OrderFactory.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.shopi.shopping.factories;
-/**
- * Factory class for creating different types of orders.
- *
- */
-public class OrderFactory {
-}
--- ShopiShopping/src/main/java/com/shopi/shopping/interfaces/DiscountStrategy.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.shopi.shopping.interfaces;
-
-import main.java.com.shopi.shopping.models.Discount;
-import main.java.com.shopi.shopping.models.products.Product;
-
-public interface DiscountStrategy {
-    double applyProductDiscount(Product product, Discount discount);
-}
