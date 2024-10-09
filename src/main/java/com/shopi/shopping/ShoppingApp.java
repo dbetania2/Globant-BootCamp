98c257de83652fc961652114a77a3e6b5ed64304
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-09 13:44:27 -0300

    Added @SpringBootApplication annotation to ShoppingApp.java

--- src/main/java/com/shopi/shopping/ShoppingApp.java
+++ src/main/java/com/shopi/shopping/ShoppingApp.java
@@ -1,15 +1,13 @@
 package com.shopi.shopping;
-import com.shopi.shopping.factories.OrderFactory;
-import com.shopi.shopping.models.Customer;
-import com.shopi.shopping.factories.ProductFactory;
-import com.shopi.shopping.models.ShoppingCart;
-import com.shopi.shopping.services.DiscountService;
-import com.shopi.shopping.services.ShoppingCartServices;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
 
+@SpringBootApplication
 public class ShoppingApp {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingApp.class, args);
     }
 }
