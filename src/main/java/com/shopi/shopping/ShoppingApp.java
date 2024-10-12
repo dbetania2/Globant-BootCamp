ca2ab98e8e85772729a7eca8ccd431e68e33cb51
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-12 06:30:11 -0300

    Enable JPA repositories in ShoppingApp

--- src/main/java/com/shopi/shopping/ShoppingApp.java
+++ src/main/java/com/shopi/shopping/ShoppingApp.java
@@ -1,10 +1,12 @@
 package com.shopi.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.shopi.shopping.repositories")
 public class ShoppingApp {
 
     public static void main(String[] args) {
