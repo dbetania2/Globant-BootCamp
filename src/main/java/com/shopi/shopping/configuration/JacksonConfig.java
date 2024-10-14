0bcd48ec23a4f44833d5085b0a17348e869d6ef6
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-14 12:56:40 -0300

    Removed code from JacksonConfig.java in preparation for deleting the configuration folder

--- src/main/java/com/shopi/shopping/configuration/JacksonConfig.java
+++ src/main/java/com/shopi/shopping/configuration/JacksonConfig.java
@@ -1,22 +1,7 @@
 package com.shopi.shopping.configuration;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // Indicates that this class contains Spring configuration
 public class JacksonConfig {
 
-    // Defines a bean for ObjectMapper which will be used for JSON serialization/deserialization
-    @Bean
-    public ObjectMapper objectMapper() {
-        // Create a new instance of ObjectMapper
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        // Register the JavaTimeModule to handle Java 8 date and time types
-        objectMapper.registerModule(new JavaTimeModule());
-
-        // Return the configured ObjectMapper
-        return objectMapper;
-    }
 }
