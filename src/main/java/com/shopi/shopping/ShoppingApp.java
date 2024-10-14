8a01afa4c353cbba882278edbd1ea4b0014a2d3c
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-14 02:05:47 -0300

    Added @EnableCaching annotation in ShoppingApp.java

--- src/main/java/com/shopi/shopping/ShoppingApp.java
+++ src/main/java/com/shopi/shopping/ShoppingApp.java
@@ -1,12 +1,14 @@
 package com.shopi.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.shopi.shopping.repositories")
+@EnableCaching
 public class ShoppingApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApp.class, args);
