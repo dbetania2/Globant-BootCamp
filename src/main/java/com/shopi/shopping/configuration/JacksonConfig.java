2ce3ddaadabccb9ca80e272982b061bae1266cf4
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-14 13:03:29 -0300

    Remove JacksonConfig as it's no longer needed for configuration

--- src/main/java/com/shopi/shopping/configuration/JacksonConfig.java
+++
@@ -1,7 +0,0 @@
-package com.shopi.shopping.configuration;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration // Indicates that this class contains Spring configuration
-public class JacksonConfig {
-
-}
