f8cf2f5e09d4e91c1cf27ec578050e514bf3a233
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-15 11:45:43 -0300

    Simplified security configuration for easier access

--- src/main/java/com/shopi/shopping/security/SecurityConfig.java
+++ src/main/java/com/shopi/shopping/security/SecurityConfig.java
@@ -1,38 +1,19 @@
 package com.shopi.shopping.security;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${spring.security.user.name}")
-    private String username;
-
-    @Value("${spring.security.user.password}")
-    private String password;
-
-    @Value("${spring.security.user.roles}")
-    private String roles;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable()) // Deshabilitar la protección CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**").permitAll() // Allows access to Actuator endpoints
-                        .anyRequest().hasRole("ADMIN") // Only users with the ADMIN role can access other routes
-                )
-                .formLogin(form -> form
-                        .loginPage("/login") // Custom login page
-                        .defaultSuccessUrl("/home", true) // Redirects to /home after a successful login
-                        .permitAll() // Allows everyone to access the login page
-                )
-                .logout(logout -> logout
-                        .permitAll() // Allows everyone to log out
+                        .anyRequest().permitAll() // Permitir todas las solicitudes sin autenticación
                 );
 
         return http.build();
