afe7a19982b13d42201e0307bf2c9a61d8feb3aa
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-15 11:45:34 -0300

    Commented out Notification.java as it is not being used

--- src/main/java/com/shopi/shopping/models/Notification.java
+++ src/main/java/com/shopi/shopping/models/Notification.java
@@ -3,41 +3,5 @@ package com.shopi.shopping.models;
 import java.io.Serializable;
 
 public class Notification implements Serializable {
-    private String message;
-    private String destination; // Can be "SMS" or "email"
-    private String recipient;    // Email address or phone number
 
-    // Default constructor required for deserialization
-    public Notification() {}
-
-    public Notification(String message, String destination, String recipient) {
-        this.message = message;
-        this.destination = destination;
-        this.recipient = recipient;
-    }
-
-    // Getters and Setters
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getRecipient() {
-        return recipient;
-    }
-
-    public void setRecipient(String recipient) {
-        this.recipient = recipient;
-    }
 }
