65a1585a6b77debcaa595e5631b44d4d827c9406
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2024-10-16 09:28:06 -0300

    Updated SecurityConfig to improve accessibility

--- src/main/java/com/shopi/shopping/security/SecurityConfig.java
+++ src/main/java/com/shopi/shopping/security/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.shopi.shopping.security;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,29 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${spring.security.user.name}")
-    private String username;
-
-    @Value("${spring.security.user.password}")
-    private String password;
-
-    @Value("${spring.security.user.roles}")
-    private String roles;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF protection
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**").permitAll() // Allows access to Actuator endpoints
-                        .anyRequest().hasRole("ADMIN") // Only users with the ADMIN role can access other routes
-                )
-                .formLogin(form -> form
-                        .loginPage("/login") // Custom login page
-                        .defaultSuccessUrl("/home", true) // Redirects to /home after a successful login
-                        .permitAll() // Allows everyone to access the login page
-                )
-                .logout(logout -> logout
-                        .permitAll() // Allows everyone to log out
+                        .anyRequest().permitAll() // Allow all requests without authentication
                 );
 
         return http.build();
